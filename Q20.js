a1bb23aee9a51a9345b6fdf2f89bf507eb6963b7
Aliza Moin <160245045+aliza-moin18@users.noreply.github.com>
2024-02-27 00:50:39 +0500

    Add files via upload

---
+++ Q20.js
@@ -0,0 +1,6 @@
+// 20 .Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+var countries = ["Pakistan", "China", "Europe", "South Korea", "Nepal", "Japan", "Russia", "Germany", "Canada", "Brazil"];
+console.log("LIST OF COUNTRIES:");
+countries.forEach(function (country) {
+    console.log(country);
+});
---
+++ Q20.ts
@@ -0,0 +1,10 @@
+// 20 .Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+ 
+let countries: string[] = ["Pakistan", "China", "Europe", "South Korea", "Nepal", "Japan", "Russia", "Germany", "Canada", "Brazil" ];
+
+console.log("LIST OF COUNTRIES:")
+
+countries.forEach(country => {
+    console.log(country)
+
+});
